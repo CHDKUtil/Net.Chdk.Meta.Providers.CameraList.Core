b2b8071f3b75b4206c9b1dacddba074301ee7715
Dmitry Shechtman <damnian@gmail.com>
2017-07-07 01:09:33 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Providers.CameraList.Core/ICameraListProvider.cs
@@ -0,0 +1,11 @@
+﻿using Net.Chdk.Meta.Model.CameraList;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Net.Chdk.Meta.Providers.CameraList
+{
+    interface ICameraListProvider
+    {
+        IDictionary<string, ListPlatformData> GetCameraList(Stream stream);
+    }
+}
