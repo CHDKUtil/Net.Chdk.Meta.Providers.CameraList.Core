1d7f4a3a7713c6d279a21624e6890d821ebb733e
Dmitry Shechtman <damnian@gmail.com>
2017-07-08 06:32:28 +0300

    Replace stream with path

--- src/Net.Chdk.Meta.Providers.CameraList.Core/ICameraListProvider.cs
+++ src/Net.Chdk.Meta.Providers.CameraList.Core/ICameraListProvider.cs
@@ -1,11 +1,10 @@
 ﻿using Net.Chdk.Meta.Model.CameraList;
 using System.Collections.Generic;
-using System.IO;
 
 namespace Net.Chdk.Meta.Providers.CameraList
 {
     public interface ICameraListProvider
     {
-        IDictionary<string, ListPlatformData> GetCameraList(Stream stream);
+        IDictionary<string, ListPlatformData> GetCameraList(string path);
     }
 }
