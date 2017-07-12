0469d33fbe49dabcdfa47e22334cba6b5be09be8
Dmitry Shechtman <damnian@gmail.com>
2017-07-12 19:51:38 +0300

    Add IInnerCameraListProvider

---
+++ src/Net.Chdk.Meta.Providers.CameraList.Core/IInnerCameraListProvider.cs
@@ -0,0 +1,11 @@
+﻿using Net.Chdk.Meta.Model.CameraList;
+using System.Collections.Generic;
+
+namespace Net.Chdk.Meta.Providers.CameraList
+{
+    public interface IInnerCameraListProvider
+    {
+        IDictionary<string, ListPlatformData> GetCameraList(string path, string categoryName);
+        string Extension { get; }
+    }
+}
